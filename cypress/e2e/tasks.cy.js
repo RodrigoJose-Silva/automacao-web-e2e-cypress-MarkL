0ab536798f4548cd04705695277077471f4d39c7
Rodrigo Jose <digo.1903@hotmail.com>
2023-04-03 21:36:15 -0300

    first test

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,9 +2,22 @@
 
 describe('tasks', () => {
     it('Should be register a new task', () => {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Ler um livro de node.js' }
+        }).then(response => {
+            expect(response.status).to.eql(204)
+        })
+
         cy.visit('http://localhost:3000/')
 
         cy.get('input[placeholder="Add a new Task"]')
             .type('Ler um livro de node.js')
+
+        cy.contains('button', 'Create').click()
+
+        cy.contains('main div p', 'Ler um livro de node.js')
+            .should('be.visible')
     })
 })
