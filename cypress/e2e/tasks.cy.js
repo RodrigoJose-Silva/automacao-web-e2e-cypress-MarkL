7913c4b2c0135ccdee72c4cca8b2765af70290d0
Rodrigo Jose <digo.1903@hotmail.com>
2023-04-09 16:13:17 -0300

    organization by context

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,35 +1,37 @@
 ///<reference types="cypress" />
 
 describe('tasks', () => {
-    it('Should be register a new task', () => {
+    context('register', () => {
+        it('Should be register a new task', () => {
 
-        const taskName = 'Ler um livro de node.js'
+            const taskName = 'Ler um livro de node.js'
 
-        cy.removeTaskByName(taskName)
-        cy.createTask(taskName)
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
 
-        cy.contains('main div p', taskName)
-            .should('be.visible')
-    })
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
 
-    it('Should not allow dulpicated task', () => {
+        it('Should not allow dulpicated task', () => {
 
-        const task = {
-            name: 'Task automated - Ler um livro de JavaScript',
-            is_done: false
-        }
+            const task = {
+                name: 'Task automated - Ler um livro de JavaScript',
+                is_done: false
+            }
 
-        cy.removeTaskByName(task.name)
-        cy.createNewTaskByAPI(task)
-        cy.createTask(task.name)
+            cy.removeTaskByName(task.name)
+            cy.createNewTaskByAPI(task)
+            cy.createTask(task.name)
 
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
 
-    it('Should be displayed filds required', ()=> {
-        cy.createTask()
-        cy.isRequired('This is a required field')
+        it('Should be displayed filds required', () => {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
     })
 })
