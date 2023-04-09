53fa3fc70ba5b561ca5c9e64a4a22e696268acfc
Rodrigo Jose <digo.1903@hotmail.com>
2023-04-09 14:58:34 -0300

    new case test

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -20,4 +20,34 @@ describe('tasks', () => {
         cy.contains('main div p', 'Ler um livro de node.js')
             .should('be.visible')
     })
+
+    it('Should not allow dulpicated task', ()=> {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Task automated - Ler um livro de JavaScript' }
+        }).then(response => {
+            expect(response.status).to.eql(204)
+        })
+        
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: { name: 'Task automated - Ler um livro de JavaScript', is_done: false }
+        }).then(response => {
+            expect(response.status).to.eql(201)
+        })
+
+        cy.visit('http://localhost:3000/')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Task automated - Ler um livro de JavaScript')
+
+        cy.contains('button', 'Create').click()
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
 })
